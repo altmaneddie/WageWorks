539f606a60bac0417aede59870144c89eba1ae11
altmaneddie <altman.eduard@gmail.com>
2018-08-25 11:30:29 +0300

    marquee added

--- src/mains/header.js
+++ src/mains/header.js
@@ -2,20 +2,34 @@ import React from 'react';
 import './header.css';
 import logo from '../assets/logo.jpg'
 import { Link, NavLink } from 'react-router-dom';
+import Marquee from 'react-marquee';
 
 class Header extends React.Component {
+
+  bannerToString = () => {
+
+    const myBanners = Object.keys(this.props.banner);
+    const banners = this.props.banner;
+  
+    let bannerText = myBanners.map((el) => {
+      return banners[el].text
+    }).join("<  =  >");
+
+    console.log(bannerText);
+    return bannerText;
+  }
 
   render() {
 
     return (
       <div>
         <div className="logo-wrapper">
-          <Link  to="/">
+          <Link to="/">
             <img alt="ww-logo" className="logo" src={logo} />
           </Link>
         </div>
         <div className="banner">
-          <p className="banner-text">{this.props.banner.text} </p>
+          <Marquee text={this.bannerToString()} leading={80} hoverToStop={true} loop={true} className="banner-text"></Marquee>
         </div>
         <div className="menu-bar">
           <NavLink activeClassName="selected" className="navlink first-navlink" to="/teams"><span className={`text-alignment`}>Teams</span></NavLink>
