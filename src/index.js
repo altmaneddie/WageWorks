10f64d0fc5b50c1d334437e01e2a5d6fc88491c5
eduard.altman <altman.eduard@gmail.com>
2018-08-31 10:53:05 +0300

    npm instructions

--- src/index.js
+++ src/index.js
@@ -6,5 +6,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// in case of issues with app running use:
+// npm install webpack@4.5.0 html-webpack-plugin@3.2.0 --save-dev
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
